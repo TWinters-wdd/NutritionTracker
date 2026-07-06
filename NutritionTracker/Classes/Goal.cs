b17fc33366335ceb4ea4dcc930ac6d1a744ed9aa
HotGarbo04 <talmageawinters@gmail.com>
2026-07-06 12:14:01 -0600

    Goal, Player, Race Classes created, Goal class edited

---
+++ NutritionTracker/Classes/Goal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutritionTracker.Classes
+{
+    public class Goal
+    {
+        public string GoalID { get; private set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public string OwnerID { get; set; }
+
+        public Goal(string title, string description, string ownerID)
+        {
+            GoalID = Guid.NewGuid().ToString();
+            Title = title;
+            Description = description;
+            IsCompleted = false;
+            OwnerID = ownerID;
+        }
+    }
+}
--- NutritionTracker/GoalGame.xaml.cs
+++ NutritionTracker/GoalGame.xaml.cs
@@ -12,12 +12,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NutritionTracker.Classes;
 
 namespace NutritionTracker
 {
-    /// <summary>
-    /// Interaction logic for GoalGame.xaml
-    /// </summary>
+    /* 
+        Uses the Goal class, Player class, and the Race class
+        This page will display a racetrack with horses on it. Each horse represents
+        a separate online player.
+        Every time a user completes a goal, their horse moves closer to the finish line.
+     */
     public partial class GoalGame : Page
     {
         public GoalGame()
